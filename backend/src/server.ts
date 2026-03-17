e975f74aa9d3bab875f38e8761ae1cc7f48426f0
Krishna Darsh E K <krishnabackup2002@gmail.com>
2026-03-17 19:03:17 +0530

    Added typescript config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,14 +4,14 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const app = exprss();
+const app : Express = exprss();
 
 app.use(cors())
 
 app.use(json)
 
 
-const PORT = process.env.BACKEND_PORT || 5000;
+const PORT : string | number = process.env.BACKEND_PORT || 5000;
 
 app.listen(PORT,() => {
    console.log(`✔️ Server connected on ${PORT}`);
